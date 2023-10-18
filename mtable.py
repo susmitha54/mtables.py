21a873fca56eda411df7c52e2750a730dd8554f6
susmitha54 <143336106+susmitha54@users.noreply.github.com>
2023-10-18 18:22:00 +0530

    Add files via upload

---
+++ mtable.py
@@ -0,0 +1,75 @@
+'''
+
+3. Print the following. Learn how to use print with formatting
+Learn how to print ********* using formatted print
+My Tables
+Table  1
+1 * 1 = 1
+1 * 2 = 2
+1 * 3 = 3
+1 * 4 = 4
+1 * 5 = 5
+**********
+Table  2
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+**********
+Table  3
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+**********
+End of tables
+
+4.Print the following
+My Tables
+Table  10
+10 * 1 = 10
+10 * 2 = 20
+10 * 3 = 30
+10 * 4 = 40
+10 * 5 = 50
+**********
+Table  8
+8 * 1 = 8
+8 * 2 = 16
+8 * 3 = 24
+8 * 4 = 32
+8 * 5 = 40
+**********
+Table  6
+6 * 1 = 6
+6 * 2 = 12
+6 * 3 = 18
+6 * 4 = 24
+6 * 5 = 30
+**********
+Table  4
+4 * 1 = 4
+4 * 2 = 8
+4 * 3 = 12
+4 * 4 = 16
+4 * 5 = 20
+**********
+Table  2
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+**********
+'''
+
+num=int(input("Enter the number:"))
+print("The multiplication table of:",num)
+for i in range(1,num+1):
+    print("Table ",i)
+    for num in range(1,6):
+        print(i,"*",num,"=",i*num)
+        
+    print("***********")
